fe5ffb624e319e32215d419419fa4785046f1229
HNuness <henriquernunes@users.noreply.github.com>
2024-05-09 11:29:29 -0300

    projeto fokus atualizado

--- script-crud.js
+++ script-crud.js
@@ -11,19 +11,19 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-<svg>
-    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-    </svg>
-</svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
 `
 
     const paragrafo = document.createElement('p')
     paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
 
     const botao = document.createElement('button')
     const imagemBotao = document.createElement('img')
+    botao.classList.add('app_button-edit')
     imagemBotao.setAttribute('src', '/assets/edit.png')
     imagemBotao.setAttribute('alt', 'Icone de editar a tarefa')
 
