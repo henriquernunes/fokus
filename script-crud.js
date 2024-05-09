ead1d5599734f4a03998021e1a596d16e7ec5f08
HNuness <henriquerosanunes@hotmail.com>
2024-05-09 10:56:12 -0300

    projeto fokus

---
+++ script-crud.js
@@ -0,0 +1,55 @@
+const btnAddTarefa = document.querySelector('.app__button--add-task')
+const formAddTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+const ulTarefas = document.querySelector('.app__section-task-list')
+
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
+
+    const svg = document.createElement('svg')
+    svg.innerHTML = `
+<svg>
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+    </svg>
+</svg>
+`
+
+    const paragrafo = document.createElement('p')
+    paragrafo.textContent = tarefa.descricao
+
+    const botao = document.createElement('button')
+    const imagemBotao = document.createElement('img')
+    imagemBotao.setAttribute('src', '/assets/edit.png')
+    imagemBotao.setAttribute('alt', 'Icone de editar a tarefa')
+
+    botao.append(imagemBotao)
+
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+
+    return li
+}
+
+btnAddTarefa.addEventListener('click', () => {
+    formAddTarefa.classList.toggle('hidden')
+})
+
+formAddTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+})
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
